5ac01e532644391e188435ac2388a2c196ff3136
Vitor Henrique <vitor.henrique@vitorstudio.com.br>
2022-01-26 16:10:18 -0300

    subindo alteração na porta

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get('/maintenance', (req,res) => {
 const jcvPanel = require("./panel/app")
 app.use("/painel", jcvPanel);
 
-app.listen(80, ()=>{
+app.listen(8080, ()=>{
     console.log(" ")
     console.log("Servidor Rodando")
     console.log(" ")
