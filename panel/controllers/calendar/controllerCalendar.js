449049d1b2c4f7167786b59f8fb36646d8fbc33b
Vitor Henrique <vitor.henrique@vitorstudio.com.br>
2022-02-01 11:33:45 -0300

    Subindo projeto final (5)

--- panel/controllers/calendar/controllerCalendar.js
+++ panel/controllers/calendar/controllerCalendar.js
@@ -718,12 +718,12 @@ exports.createQrCode = async (req,res) => {
     var qr = require('qr-image');
 
     var qr_svg = qr.image(GLOBAL_LINK_QR+idRoom+'/?linkspecial=/painel/calendario/viewRoom/'+idRoom, { type: 'png' });
-    qr_svg.pipe(require('fs').createWriteStream('room-'+idRoom+'.png'));
+    qr_svg.pipe(require('fs').createWriteStream('../../../room-'+idRoom+'.png'));
 
     //var svg_string = qr.imageSync('I love QR!', { type: 'png' });
 
     //let fileDow = 'public/panel/qrcode-rooms/room-'+idRoom+'.png';
-    let fileDow = '/home/jcv/app/public/panel/qrcode-rooms/room-'+idRoom+'.png';
+    let fileDow = '/room-'+idRoom+'.png';
 
     /* setTimeout(()=> {
         res.download(fileDow)
