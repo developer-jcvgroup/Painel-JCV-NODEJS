ec7de431040cdc800500a20aaa7f341776e6aaa0
Vitor Henrique <vitor.henrique@vitorstudio.com.br>
2022-02-01 10:24:28 -0300

    Subindo projeto final (5)

--- login/app.js
+++ login/app.js
@@ -29,7 +29,7 @@ router.get("/", async (req,res)=>{
         //Pegando todas as contas que não possui uma senha
         const allAccountPass = await database
         .select("jcv_userCpf")
-        .where({jcv_userPassword: null, jcv_userPassword: ''})
+        .whereRaw("jcv_userPassword = null OR jcv_userPassword = ''")
         .table("jcv_users")
         .then( data => {
 
