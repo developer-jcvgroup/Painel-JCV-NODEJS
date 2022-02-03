87428768b826afbf146012e2b95284add0f87c6d
Vitor Henrique <vitor.henrique@vitorstudio.com.br>
2022-02-03 09:16:53 -0300

    Subindo ajustes no home do painel sobre os eventos

--- system/systemAlertCalendar.js
+++ system/systemAlertCalendar.js
@@ -18,6 +18,8 @@ async function lembreteCalendar(){
     .select()
     .where({sys_calendar_eventMonth: monthNumber+'/'+yearMonth})
     .table("jcv_calendar_registers")
+    .join("jcv_unitys","jcv_calendar_registers.sys_calendar_eventLocation","jcv_unitys.sys_unity_id")
+    .join("jcv_calendar_rooms","jcv_calendar_registers.sys_calendar_eventRoom","jcv_calendar_rooms.sys_calendar_roomId")
     .then( data => {
         
         data.forEach(element => {
@@ -39,7 +41,7 @@ async function lembreteCalendar(){
                     });
 
                     const textOne = 'Um evento está próximo de acontecer';
-                    const textTwo = `Existe um evento que esta prestes a iniciar faltando apenas <b>${element.sys_calendar_eventReminder} minutos!</b>.<br> Nome do evento: <b>${element.sys_calendar_eventName}</b>. <br> Data do evento: <b>${element.sys_calendar_eventDate}</b>.`;
+                    const textTwo = `Existe um evento que esta prestes a iniciar faltando apenas <b>${element.sys_calendar_eventReminder} minutos!</b>.<br> Nome do evento: <b>${element.sys_calendar_eventName}</b>. <br> Data do evento: <b>${element.sys_calendar_eventDate}</b>.  <br> Sala: <b>${element.sys_calendar_roomName}</b>. <br> Local: <b>${element.sys_unity_name}</b>`;
                     emailSystemExe.sendMailExe(newArrayEamils, 'Evento Próximo', 'Evento Próximo', 'Calendario', '', textOne, textTwo);
 
                 })
@@ -59,7 +61,7 @@ async function lembreteCalendar(){
                     });
 
                     const textOne = 'Um evento está próximo de acontecer';
-                    const textTwo = `Existe um evento que esta prestes a iniciar faltando apenas <b>1 minuto!</b>.<br> Nome do evento: <b>${element.sys_calendar_eventName}</b>. <br> Data do evento: <b>${element.sys_calendar_eventDate}</b>.`;
+                    const textTwo = `Existe um evento que esta prestes a iniciar faltando apenas <b>1 minuto!</b>.<br> Nome do evento: <b>${element.sys_calendar_eventName}</b>. <br> Data do evento: <b>${element.sys_calendar_eventDate}</b>. <br> Sala: <b>${element.sys_calendar_roomName}</b>. <br> Local: <b>${element.sys_unity_name}</b>`;
                     emailSystemExe.sendMailExe(newArrayEamils, 'Evento Próximo', 'Evento Próximo', 'Calendario', '', textOne, textTwo);
 
                 })
@@ -69,11 +71,6 @@ async function lembreteCalendar(){
     })
 }
 
-/* cron.schedule('* 1 * * * *', function() {
-    console.log("Executando a tarefa a cada minuto: "+moment().format("DD-MM-YYYY HH:mm:ss"))
-    lembreteCalendar()
-});
- */
 cron.schedule('*/60 * * * * *', () => {
     console.log("Executando a tarefa a cada minuto: "+moment().format("DD-MM-YYYY HH:mm:ss"))
     lembreteCalendar()
