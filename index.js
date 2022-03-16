88e1be88063d6dc52480771a00e7a271bc078283
Vitor Henrique <vitor.henrique@vitorstudio.com.br>
2022-03-16 17:58:08 -0300

    Subindo ajustes

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const http = require('http').createServer(app);
 
-const io = require('socket.io')(http)
-io.origins(['*']);
+const io = require('socket.io')(http, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  })
 
 const database = require("./panel/database/database");
 
