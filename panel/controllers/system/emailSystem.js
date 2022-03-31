185b3436b7b89ba778dedbc0fa938862cd5ac067
Vitor Henrique <vitor.henrique@vitorstudio.com.br>
2022-03-31 16:32:53 -0300

    Subindo ajustes emails sender

--- panel/controllers/system/emailSystem.js
+++ panel/controllers/system/emailSystem.js
@@ -13,12 +13,12 @@ exports.sendMailExe = async (sysEmails, subTitleEmail, titleBodyMail, sysApp, sy
     const nodemailer = require('nodemailer');
 
     const transporter = nodemailer.createTransport({
-        host: "jcv.net.br",
+        host: "smtp.vitorstudio.com.br",
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: "nao-responda@jcv.net.br",
-            pass: "!@#naorespondajcv"
+            user: "nao-responda=vitorstudio.com.br",
+            pass: "!@#naoresponda123"
         },
         tls: { rejectUnauthorized: false },
         connectionTimeout: 5 * 60 * 1000, // 5 min
@@ -34,7 +34,7 @@ exports.sendMailExe = async (sysEmails, subTitleEmail, titleBodyMail, sysApp, sy
     });
 
     const mainOptions = {
-        from: 'Sistema | JCV GROUP nao-responda@jcv.net.br',
+        from: 'Sistema | JCV GROUP nao-responda@vitorstudio.com.br',
         to: sysEmails,
         subject: subTitleEmail,
         html: data
